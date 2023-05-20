582a2b3346458e6f7338bc9cbcb47ee6fd8ce7c8
Jacob Hummer <jcbhmr@outlook.com>
2023-05-20 16:18:14 -0500

    test according to original idea

---
+++ test/fixtures/module.js
@@ -0,0 +1,6 @@
+export class C {}
+export function f() {}
+export const x = 45;
+export default () => {};
+const v = 11;
+export { v as y };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,32 +1,51 @@
 import { test, expect, assert } from "vitest";
-import { module, Module } from "../src/index";
+import module from "../src/index";
 
-test("is a module object", () => {
-  const mod = module(import.meta)`
-    1 + 1;
-  `;
-  expect(mod).toBeInstanceOf(Module);
+test("returns something that's import()-able", async () => {
+  const mod = module(import.meta, async () => {});
+  const n = await import(mod);
+  expect(n).toBeDefined();
 });
 
-test("works with imports", async () => {
-  const mod = module(import.meta)`
-    import {} from "node:http";
-  `;
-  expect(await import(mod)).toBeDefined();
+test("returns the same thing when imported twice", async () => {
+  const mod = module(import.meta, async () => {});
+  const n1 = await import(mod);
+  const n2 = await import(mod);
+  expect(n).toBe(n2);
 });
 
-test("works with exports", async () => {
-  const mod = module(import.meta)`
-    export const a = 45;
-    export const b = "Hi!";
-  `;
-  expect(await import(mod)).toMatchObject({ a: 45, b: "Hi!" });
+test("can export things", async () => {
+  const mod = module(import.meta, () => {
+    return { a: 1, b: 2 };
+  });
+  const n = await import(mod);
+  expect(n).toMatchObject({ a: 1, b: 2 });
 });
 
-test("works with npm packages", async () => {
-  const mod = module(import.meta)`
-    import {} from "vite";
-    import {} from "vitest";
-  `;
-  expect(await import(mod)).toBeDefined();
+test("can import things", async () => {
+  const mod = module(import.meta, async () => {
+    const { Readable } = await import("node:stream");
+    const { test } = await import("vitest");
+  });
+  const n = await import(mod);
+  expect(n).toBeDefined();
+});
+
+test("can import local files", async () => {
+  const mod = module(import.meta, async () => {
+    const {} = await import("./fixtures/module.js");
+  });
+  const n = await import(mod);
+  expect(n).toBeDefined();
+});
+
+test("import.meta is correct", async () => {
+  const mod = module(import.meta, async () => {
+    return { default: import.meta };
+  });
+  const { default: importMeta } = await import(mod);
+  expect(importMeta.url).toBe(import.meta.url);
+  expect(importMeta.resolve("./fixtures/module.js")).toBe(
+    import.meta.resolve("./fixtures/module.js")
+  );
 });
